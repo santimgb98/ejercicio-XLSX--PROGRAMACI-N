73e36a7cff8b6cc287bdce64e3e13128593df4ef
santimgb98 <santimgb98@email.com>
2025-05-12 17:09:30 +0200

    estructura hecha, falta editar archivo xlsx y listar los archivos correctamente

---
+++ js/classes.js
@@ -0,0 +1,60 @@
+const xlsx = require('xlsx');
+const prompt = require("prompt-sync")();
+const fs = require('fs');
+const { log } = require('console');
+const workbook = xlsx.utils.book_new();
+
+class Menu{
+
+    crear_archivo(){
+        
+        // Crear libro nuevo
+        const workbook = xlsx.utils.book_new();
+        const nombre_libro_nuevo = prompt("Introduce el nombre del nuevo libro de excel: ");
+        
+        // Contenido celda A1
+        xlsx.utils.book_append_sheet(workbook,xlsx.utils.aoa_to_sheet([['Header']]))
+
+        // Creación del archivo xlsx
+        xlsx.writeFile(workbook, `./xlsx/${nombre_libro_nuevo}.xlsx`);
+        console.clear()
+    }
+    listar_archivos(){
+        fs.readdir('./xlsx',(err,files)=>{
+            if(err){
+                log("No se ha podido acceder a los archivos");
+                return;
+            }
+            log('Carpeta XLSX: ',`${files}`);
+        })
+    }
+    leer_archivo(){
+        const excel = xlsx.readFile("./xlsx/libro2.xlsx");
+        const nombreHoja = excel.SheetNames;
+
+        let datos = xlsx.utils.sheet_to_csv(excel.Sheets[nombreHoja[0]],{
+            cellDates : true
+        })
+        log(datos);
+       
+    }
+    borrar_archivo(){
+        fs.unlinkSync("./xlsx/libro3.xlsx");
+        console.clear()
+    }
+    editar_archivo(){
+        var sheet = workbook.Sheets['libro1'];
+
+        // Modificar la celda A1
+        sheet[0] = 'Nuevo valor';
+
+        // Agregar una nueva fila
+        var nuevaFila = ['Nueva fila', 'valor 1', 'valor 2'];
+        xlsx.utils.sheet_add_aoa(sheet, [nuevaFila], {start: 'B3'});
+
+        // Guardar los cambios
+        xlsx.writeFile(workbook, "./xlsx/libro1.xlsx");
+    }
+}
+
+module.exports = Menu;
